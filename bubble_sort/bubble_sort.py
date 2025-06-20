ab1771e5dde6b011e1e53e78b5b0885df89a0424
BWahot <benjaminwahothi04@gmail.com>
2025-06-20 09:45:34 +0300

    bubble sort  and selection sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,14 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    pass
+    n= len(unsorted_list)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if unsorted_list[j] > unsorted_list[j+1]:
+                # swap if the element found is greater than the next element
+                unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
+    return unsorted_list
+
+my_list= [64, 34, 25, 12, 22, 11, 90]
+print("Unsorted list:", my_list)
+bubble_sort(my_list)
+print("Sorted list:", my_list)
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,15 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+    n=len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+my_list = [34, 4, 25, 12, 2, 1, 9]
+print("Unsorted list:", my_list)
+selection_sort(my_list)
+print("Sorted list:", my_list)
